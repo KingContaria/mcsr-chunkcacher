d9d51fa0ff5322e8435569bf1ac24d866e003dc2
char <45241413+char3210@users.noreply.github.com>
2024-04-10 10:04:23 -0500

    correct but slower (todo don't serialize)

--- src/main/java/me/char321/chunkcacher/WorldCache.java
+++ src/main/java/me/char321/chunkcacher/WorldCache.java
@@ -9,8 +9,10 @@ import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.ChunkSerializer;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.gen.GeneratorOptions;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,21 +20,27 @@ import java.util.Map;
 public class WorldCache {
     public static boolean isGenerating = false;
     private static GeneratorOptions lastGeneratorOptions;
-    private static final Map<RegistryKey<World>, Long2ObjectLinkedOpenHashMap<NbtCompound>> cache = new HashMap<>();
+    private static final Map<RegistryKey<World>, List<Long2ObjectLinkedOpenHashMap<NbtCompound>>> cache = new HashMap<>();
     public static List<ChunkPos> strongholdCache;
 
-    public static void addChunk(ChunkPos chunkPos, Chunk chunk, ServerWorld world) {
-        cache.computeIfAbsent(world.getRegistryKey(), k -> new Long2ObjectLinkedOpenHashMap<>()).put(chunkPos.toLong(), ChunkSerializer.serialize(world, chunk));
+    public static void addChunk(ChunkPos chunkPos, ChunkStatus status, Chunk chunk, ServerWorld world) {
+        cache.computeIfAbsent(world.getRegistryKey(), k -> {
+            List<Long2ObjectLinkedOpenHashMap<NbtCompound>> list = new ArrayList<>();
+            for (int i = 0; i < 9; i++) {
+                list.add(new Long2ObjectLinkedOpenHashMap<>());
+            }
+            return list;
+        }).get(status.getIndex()).put(chunkPos.toLong(), ChunkSerializer.serialize(world, chunk));
     }
 
     public static boolean shouldCache() {
         return isGenerating && Atum.isRunning;
     }
 
-    public static NbtCompound getChunkNbt(ChunkPos chunkPos, ServerWorld world) {
-        Long2ObjectLinkedOpenHashMap<NbtCompound> map = cache.get(world.getRegistryKey());
-        if (map == null) return null;
-        return map.get(chunkPos.toLong());
+    public static NbtCompound getChunkNbt(ChunkPos chunkPos, ChunkStatus status, ServerWorld world) {
+        List<Long2ObjectLinkedOpenHashMap<NbtCompound>> list = cache.get(world.getRegistryKey());
+        if (list == null) return null;
+        return list.get(status.getIndex()).get(chunkPos.toLong());
     }
 
     /**
